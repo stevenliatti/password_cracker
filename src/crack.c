69062705507cc2580dcc321dddfab71bccac0e41
orphee.antoniad <orphee.antoniadis@etu.hesge.ch>
2016-10-13 15:58:17 +0200

    Choses faites : - Création d'une structure contenant les variables à passer dans les threads - Implémentation de la fonction 'init_seedchars' qui initialise le tableau des caractères possibles dans le mot de passe - Initialisation de la structure dans le main et passage en argument au thread Choses à faire : - Réfléchir à comment on va gérer les threads pour cracker le mot de passe rapidement - Faire une fonction de libération mémoire de la structure (ou trouver un moyen de l'initialiser san malloc)

--- src/crack.c
+++ src/crack.c
@@ -5,38 +5,62 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+typedef struct passwd_st {
+	char* hash;
+	char* salt;
+	char* seedchars;
+	int threads_nb;
+	struct crypt_data* cdata;
+} passwd_st;
+
+char* init_seedchars() {
+	char* seedchars = malloc(sizeof(char)*65);
+	for (int i = 0; i < 10; i++) {
+		seedchars[i] = i + '0';
+	}
+	for (int i = 0; i < 26; i++) {
+		seedchars[i+10] = i + 'A';
+		seedchars[i+36] = i + 'a';
+	}
+	seedchars[62] = '!';
+	seedchars[63] = '*';
+	seedchars[64] = '~';
+	return seedchars;
+}
+
 void* thread(void* arg) {
+	passwd_st* temp = (passwd_st*)arg;
+	// test de la fonction crypt_r
+	char* hash = crypt_r("Steven aime sucer", temp->salt, temp->cdata);
+	printf("%s\n",hash);
 	return NULL;
 }
 
 int main(int argc, char** argv) {
 	if (argc == 4) {
 		// initialisation des variables
-		char* hash = argv[1];
-		char* salt = argv[2];
-		int threads_nb = atoi(argv[3]);
-
-		// test de la fonction crypt_r
-		struct crypt_data* cdata = (struct crypt_data *)malloc(sizeof(struct crypt_data));
-		cdata->initialized = 0;
-		char* my_hash = crypt_r("Steven aime sucer", salt, cdata);
-		printf("%s\n",my_hash);
+		passwd_st* passwd = malloc(sizeof(passwd_st));
+		passwd->hash = argv[1];
+		passwd->salt = argv[2];
+		passwd->seedchars = init_seedchars();
+		passwd->threads_nb = atoi(argv[3]);
+		passwd->cdata = (struct crypt_data *)malloc(sizeof(struct crypt_data));
+		passwd->cdata->initialized = 0;
 
 		// initialisation des threads
-		pthread_t t[threads_nb];
-		for (int i = 0; i < threads_nb; i++) {
-			if (pthread_create(&t[i], NULL, thread, NULL) != 0) {
+		pthread_t t[passwd->threads_nb];
+		for (int i = 0; i < passwd->threads_nb; i++) {
+			if (pthread_create(&t[i], NULL, thread, passwd) != 0) {
 				fprintf(stderr, "pthread_create failed!\n");
 				return EXIT_FAILURE;
 			}
 		}
-		for (int i = 0; i < threads_nb; i++) {
+		for (int i = 0; i < passwd->threads_nb; i++) {
 			if (pthread_join(t[i], NULL) != 0) {
 				fprintf(stderr, "pthread_join failed!\n");
 				return EXIT_FAILURE;
 			}
 		}
-		free(cdata);
 		return EXIT_SUCCESS;
 	} else {
 		fprintf(stderr,"Invalid argument count !\n");
