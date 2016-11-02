5875c6bb715ed00506b1584184c5e89026159792
steven.liatti <steven.liatti@etu.hesge.ch>
2016-11-02 11:25:37 +0100

    Enfin ! J'arrive à générer un mot de passe en fonction de sa position dans l'espace des combinaisons. Faut que je commente et je teste d'avantage, mais ça marche en tout cas avec un alphabet de 2, 3 et char sur les 100 premières positions.

--- src/test_steven/test_gen_str.c
+++ src/test_steven/test_gen_str.c
@@ -7,10 +7,16 @@
 #include <stdbool.h>
 #include <assert.h>
 
-const char* alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!*~";
-const int alphabet_size = 65;
-const int passwd_size = 8;
+// const char* alphabet = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!*~";
+const char* alphabet = "abcd";
+const int alphabet_size = 3;
+const int passwd_size = 4;
 
+/**
+ * Cette fonction retourne la longueur d'un mot de passe
+ * en fonction de sa position dans l'espace des possibilités
+ * ou combinaisons de mots de passe (en fonction de l'alphabet).
+ */
 int pass_len(long position) {
 	int i = 1;
 	long min = 0;
@@ -25,8 +31,13 @@ int pass_len(long position) {
 	return i;
 }
 
-long sub_index(int pass_len_actual, int pass_len_choose, long position) {
-	if (pass_len_actual == pass_len_choose) {
+/**
+ * Cette fonction retourne une position précédente ou se trouve le
+ * même pattern de mot de passe (ex: le mot de passe "abb" est constitué
+ * de "bb" sur les bits de poids faible).
+ */
+long sub_index(int pass_len_actual, int pass_len_previous, long position) {
+	if (pass_len_actual == pass_len_previous) {
 		return position;
 	}
 	else {
@@ -35,17 +46,27 @@ long sub_index(int pass_len_actual, int pass_len_choose, long position) {
 		while (pass_len(new_position) == pass_len_actual) {
 			new_position = new_position - past_position;
 		}
-		return sub_index(pass_len_actual - 1, pass_len_choose, new_position);
+		return sub_index(pass_len_actual - 1, pass_len_previous, new_position);
 	}
 }
 
 void gen_str(char* str, long position, int pass_len_start) {
 	int pass_len_actual = pass_len(position);
-	if (pass_len_actual == 1) {
+	if (pass_len_actual == 1)
 		str[pass_len_start - 1] = alphabet[position];
-	}
 	else {
 		long sub_position = sub_index(pass_len_actual, pass_len_actual - 1, position);
+		int index = 0;
+		long new_position = position;
+		int pass_len_past = pass_len_actual - 1;
+
+		if (pass_len_actual != pass_len_start)
+			index = pass_len_start - pass_len_actual;
+
+		for (int i = pass_len_past; i > 0; i--)
+			new_position = new_position - (long) pow(alphabet_size, i);
+
+		str[index] = alphabet[new_position / (long) pow(alphabet_size, pass_len_past)];
 		gen_str(str, sub_position, pass_len_start);
 	}
 }
@@ -55,10 +76,11 @@ int main() {
 	cdata = malloc(sizeof(struct crypt_data));
 	cdata->initialized = 0;
 	char* str = malloc(sizeof(char) * passwd_size + 1);
-	for (int i = 0; i < alphabet_size; i++) {
-		gen_str(str, i, 1);
+	for (int i = 0; i < 100; i++) {
+		gen_str(str, i, pass_len(i));
 		// printf("%s\n", crypt_r(str, "43", cdata));
 		printf("%s\n", str);
 	}
 	free(str);
+	free(cdata);
 }
